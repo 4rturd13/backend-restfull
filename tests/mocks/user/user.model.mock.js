49504a4060953a3209fbf87d49a3970f5902ca93
4rturd13 <4rturd13@gmail.com>
2020-10-08 16:15:27 -0500

    ADD: structure of project and models

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: '507f191e810c19729de860ea',
+    name: '4rturd13',
+    username: 'excalibur',
+    password: 'mystrongPassword',
+  },
+  users: [
+    {
+      _id: '507f191e810c19729de860ea',
+      name: '4rturd13',
+      username: 'excalibur',
+      password: 'mystrongPassword',
+    },
+    {
+      _id: '507f191e810c19729de860eb',
+      name: 'Erick',
+      username: 'Erick_34',
+      password: 'mystrongPassword',
+    },
+  ],
+}
